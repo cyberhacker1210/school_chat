61586c0741eddccb1713da3547fddf9ce4317258
cyberhacker1210 <leolintello21@gmail.com>
2023-09-10 12:30:55 +0200

    crossed table ok

--- site_leo/blog/views.py
+++ site_leo/blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from .models import Planning
+from django.db.models import Count
 
 # Create your views here.
 def planning(request):
     return render(request, "blog/leo.html")
 
 def index(request):
-    context = {"plannings": Planning.objects.all()}
+    jour_distinctes = Planning.objects.values('jour').annotate(Count('id')).order_by('jour')
+    context = {"plannings": Planning.objects.all(), 'jour_distinctes': jour_distinctes}
     return render(request, "blog/planning.html", context)
-
-
